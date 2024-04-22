a6ee0f5118868b102160cd60bdf78e68b3e400ec
izumii <154342465+izumiCode@users.noreply.github.com>
2024-04-22 21:47:24 +0530

    Real time notes/page title change/editing

--- app/(main)/_components/Title.tsx
+++ app/(main)/_components/Title.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { Doc } from "@/convex/_generated/dataModel"
 import { useMutation } from "convex/react"
 import { api } from "@/convex/_generated/api"
@@ -12,21 +12,60 @@ interface TitleProps {
 }
 
 export default function Title({ initialData }: TitleProps) {
+    const inputRef = useRef<HTMLInputElement>(null)
     const update = useMutation(api.documents.update)
+
+    const [title, setTitle] = useState(initialData.title || 'untitled.')
     const [isEditing, setIsEditing] = useState(false)
+
+    const enableInput = () => {
+        setTitle(initialData.title)
+        setIsEditing(true)
+        setTimeout(() => {
+            inputRef.current?.focus();
+            inputRef.current?.setSelectionRange(0, inputRef.current.value.length)
+        }, 0)
+    }
+
+    const disableInput = () => {
+        setIsEditing(false)
+    }
+
+    const onChange = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setTitle(e.target.value)
+        update({
+            id: initialData._id,
+            title: e?.target.value || "untitled"
+        })
+    }
+
+    const onKeyDown = (
+        event: React.KeyboardEvent<HTMLInputElement>
+    ) => {
+        if (event.key == "Enter")
+            disableInput()
+    }
 
     return (
         <div className="flex items-center gap-x-1 ">
             {!!initialData?.icon && <p>{initialData.icon}</p>}
             {isEditing ? (
                 <Input
+                    ref={inputRef}
+                    onClick={enableInput}
+                    onBlur={disableInput}
+                    onChange={onChange}
+                    onKeyDown={onKeyDown}
+                    value={title}
                     className="h-7 px-2 focus-visible:ring-transparent"
 
                 />
             ) : (
                 <Button
                     className="font-normal h-auto p-1"
-                    onClick={() => { }}
+                    onClick={enableInput}
                     variant={"ghost"}
                     size={"sm"}
                 >
